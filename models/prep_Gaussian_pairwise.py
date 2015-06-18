39436dde02d3038f25363735f8093d89b947e83d
Alexander Kluber <ajkluber@rice.edu>
2015-06-18 16:46:38 -0500

    [models/prep_Gaussian_pairwise.py] Added option for random native heterogeneous contacts.

--- models/prep_Gaussian_pairwise.py
+++ models/prep_Gaussian_pairwise.py
@@ -18,9 +18,12 @@ TODO:
 
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 
 import pdb_parser
 import residue_properties as rp
+
+
 
 #
 #    count = 0
@@ -35,7 +38,7 @@ import residue_properties as rp
 #                C[j,i] = eps_random[count]
 #                count += 1
 
-def add_nonnative_interactions(args,pdb,native_pairs,pairwise_param_file_string,model_param_file_string,model_param,random=False):
+def add_random_nonnative_interactions(args,pdb,native_pairs,pairwise_param_file_string,model_param_file_string,model_param,random=False):
 
     nn_cutoff = 0.85
 
@@ -54,7 +57,7 @@ def add_nonnative_interactions(args,pdb,native_pairs,pairwise_param_file_string,
                 res_i = res_types[i]
                 res_j = res_types[j]
                 if random:
-                    gamma = np.random.normal(loc=args.nonnative_mean,scale=np.sqrt(args.nonnative_var))
+                    gamma = np.random.normal(loc=args.mean,scale=np.sqrt(args.var))
                 else:
                     gamma = rp.get_awsem_direct_contact_gamma(res_i,res_j)
                 eps_nn.append(gamma)
@@ -65,8 +68,8 @@ def add_nonnative_interactions(args,pdb,native_pairs,pairwise_param_file_string,
     eps_nn_avg = np.mean(eps_nn)
     eps_nn_var = np.var(eps_nn)
     eps_nn -= eps_nn_avg
-    eps_nn *= np.sqrt(args.nonnative_var/eps_nn_var)
-    eps_nn += args.nonnative_mean
+    eps_nn *= np.sqrt(args.var/eps_nn_var)
+    eps_nn += args.mean
 
     rNC_nn = 0.4
     r0_nn = rNC_nn + 0.1
@@ -126,12 +129,18 @@ def get_Gaussian_pairwise(pdb,pairs,model_param=0):
 
     return pairwise_param_file_string, model_param_file_string
 
-def get_compound_LJ12_Gaussian_pairwise(pdb,pairs,model_param=0):
+def get_compound_LJ12_Gaussian_pairwise(args,pdb,pairs,model_param=0):
     """ """
 
     width0 = 0.05
     pairwise_distances = pdb_parser.get_pairwise_distances(pdb,pairs)
-    model_param_value = 1.
+    pdb_info = pdb_parser.get_coords_atoms_residues(pdb)
+    n_residues = len(pdb_info[4]) 
+    C_native = np.zeros((n_residues,n_residues),float)
+    if args.random_native:
+        epsilons = np.random.normal(loc=args.avg_native_eps,scale=args.var,size=len(pairs))
+    else:
+        epsilons = np.ones(len(pairs),float)
     pairs_rNC = 0.4*np.ones(len(pairs))
 
     # Loop over pairs and create pair
@@ -145,28 +154,71 @@ def get_compound_LJ12_Gaussian_pairwise(pdb,pairs,model_param=0):
         j_idx = pairs[i][1]
         r0 = pairwise_distances[i] 
         rNC = pairs_rNC[i]
+        gamma = epsilons[i]
+        
+        C_native[j_idx - 1, i_idx - 1] = gamma
 
         # compound_LJ12_Gaussian takes rNC, r0, width0
         LJ12_Gaussian_other_params = "%10.5f%10.5f%10.5f" % (rNC,r0,width0)
         pairwise_param_file_string += "%5d%5d%5d%5d%s\n" % (i_idx,j_idx,model_param,8,LJ12_Gaussian_other_params) 
         model_param += 1
-        model_param_file_string += "%10.5f\n" % model_param_value
+        model_param_file_string += "%10.5f\n" % 1.
+
+        if gamma < 0.:
+            interaction_type = 5
+        else: 
+            interaction_type = 4
+        model_param_value = abs(gamma)
 
         Gaussian_other_params = "%10.5f%10.5f" % (r0,width0)
-        pairwise_param_file_string += "%5d%5d%5d%5d%s\n" % (i_idx,j_idx,model_param,4,Gaussian_other_params) 
+        pairwise_param_file_string += "%5d%5d%5d%5d%s\n" % (i_idx,j_idx,model_param,interaction_type,Gaussian_other_params) 
         model_param_file_string += "%10.5f\n" % model_param_value
         model_param += 1
 
-    return pairwise_param_file_string, model_param_file_string, model_param
+    return pairwise_param_file_string, model_param_file_string, model_param, C_native
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='.')
-    parser.add_argument('--pdb', type=str, required=True, help='PDB file with structure.')
-    parser.add_argument('--pairs', type=str, required=True, help='List of pairs.')
-    parser.add_argument('--noclean',action='store_true',default=False,help='Do not clean the pdb.')
-    parser.add_argument('--nonnative', action='store_true', default=False, help='Optional. Add flavored non-native interactions.')
-    parser.add_argument('--nonnative_mean', type=float, default=None, help='Optional. Non-native interaction mean.')
-    parser.add_argument('--nonnative_var',  type=float, default=None, help='Optional. Non-native interaction std dev.')
+    parser.add_argument('--pdb', 
+                        type=str, 
+                        required=True, 
+                        help='PDB file with structure.')
+
+    parser.add_argument('--pairs', 
+                        type=str, 
+                        required=True, 
+                        help='List of pairs.')
+    
+    parser.add_argument('--noclean',
+                        action='store_true',
+                        default=False,
+                        help='Do not clean the pdb.')
+
+    parser.add_argument('--random_native', 
+                        action='store_true', 
+                        default=False, 
+                        help='Optional. Add random native heterogeneity.')
+
+    parser.add_argument('--random_nonnative', 
+                        action='store_true', 
+                        default=False, 
+                        help='Optional. Add random non-native interactions.')
+
+    parser.add_argument('--avg_native_eps', 
+                        type=float, 
+                        default=1., 
+                        help='Optional. Average native interaction strength.')
+    
+    parser.add_argument('--mean', 
+                        type=float, 
+                        default=None, 
+                        help='Optional. Non-native interaction mean.')
+
+    parser.add_argument('--var',  
+                        type=float, 
+                        default=None, 
+                        help='Optional. Non-native interaction std dev.')
+
     args = parser.parse_args()
 
     pdbfile = args.pdb
@@ -182,16 +234,54 @@ if __name__ == "__main__":
     else:
         pdb = open(pdbfile,"r").read()
 
-    pairwise_param_file_string, model_param_file_string, model_param = get_compound_LJ12_Gaussian_pairwise(pdb,pairs)
+    pairwise_param_file_string, model_param_file_string, model_param, C_native = get_compound_LJ12_Gaussian_pairwise(args,pdb,pairs)
 
-    if args.nonnative:
-        if (args.nonnative_mean is None) or (args.nonnative_var is None):
-            raise IOError("Need to specify --nonnative_mean --nonnative_var for nonnative interactions.")
-        pairwise_param_file_string, model_param_file_string, eps_nn, C_nn = add_nonnative_interactions(args,pdb,pairs,pairwise_param_file_string,model_param_file_string,model_param,random=True)
+    cmap = plt.get_cmap("jet")
+    cmap.set_bad(color="gray",alpha=1.)
+    if args.random_nonnative:
+        if (args.mean is None) or (args.var is None):
+            raise IOError("Need to specify --mean --var for random_nonnative interactions.")
+        pairwise_param_file_string, model_param_file_string, eps_nn, C_nn = add_random_nonnative_interactions(args,pdb,pairs,pairwise_param_file_string,model_param_file_string,model_param,random=True)
 
-    if args.nonnative:
-        open("%s_pairwise_params_%.2f_%.2f" % (name,args.nonnative_mean,args.nonnative_var),"w").write(pairwise_param_file_string)
-        open("%s_model_params_%.2f_%.2f" % (name,args.nonnative_mean,args.nonnative_var),"w").write(model_param_file_string)
+        C_nn = np.ma.masked_invalid(C_nn)
+        plt.figure()
+        plt.pcolormesh(C_nn,cmap=cmap)
+        plt.xlim(0,C_nn.shape[0])
+        plt.ylim(0,C_nn.shape[0])
+        plt.xlabel("Residue i")
+        plt.ylabel("Residue j")
+        cbar = plt.colorbar()
+        cbar.set_label("Interaction strength")
+        plt.title("%s random non-native  $\\overline{\\epsilon_{nn}} = %.2f$ $\\sigma_{\\epsilon_{nn}}^2 = %.2f$ " \
+                    % (name,args.mean,args.var))
+        plt.savefig("%s_random_%.2f_%.2f.png" % (name,args.mean,args.var),format="png")
+        plt.savefig("%s_random_%.2f_%.2f.pdf" % (name,args.mean,args.var),format="pdf")
+        plt.savefig("%s_random_%.2f_%.2f.eps" % (name,args.mean,args.var),format="eps")
+        plt.show()
+
+    if args.random_native:
+        plt.figure()
+        plt.pcolormesh(C_native,cmap=cmap)
+        plt.xlim(0,C_native.shape[0])
+        plt.ylim(0,C_native.shape[0])
+        plt.xlabel("Residue i")
+        plt.ylabel("Residue j")
+        cbar = plt.colorbar()
+        cbar.set_label("Interaction strength")
+        plt.title("%s random native  $\\overline{\\epsilon_{nat}} = %.2f$ $\\sigma_{\\epsilon_{nat}}^2 = %.2f$ " \
+                    % (name,args.avg_native_eps,args.var))
+        plt.savefig("%s_random_native_%.2f_%.2f.png" % (name,args.avg_native_eps,args.var),format="png")
+        plt.savefig("%s_random_native_%.2f_%.2f.pdf" % (name,args.avg_native_eps,args.var),format="pdf")
+        plt.savefig("%s_random_native_%.2f_%.2f.eps" % (name,args.avg_native_eps,args.var),format="eps")
+        plt.show()
+
+
+    if args.random_nonnative:
+        open("%s_pairwise_params_%.2f_%.2f" % (name,args.mean,args.var),"w").write(pairwise_param_file_string)
+        open("%s_model_params_%.2f_%.2f" % (name,args.mean,args.var),"w").write(model_param_file_string)
+    elif args.random_native:
+        open("%s_pairwise_params_%.2f_%.2f" % (name,args.avg_native_eps,args.var),"w").write(pairwise_param_file_string)
+        open("%s_model_params_%.2f_%.2f" % (name,args.avg_native_eps,args.var),"w").write(model_param_file_string)
     else:
         open("%s_pairwise_params" % name,"w").write(pairwise_param_file_string)
         open("%s_model_params" % name,"w").write(model_param_file_string)
