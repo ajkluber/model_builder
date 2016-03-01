097d76dad659237be532741d01752b7448dafd95
Alexander Kluber <ajkluber@rice.edu>
2016-03-01 10:27:18 -0600

    [Model,pair_potentials,potentials] potentials handled by the Hamiltonian class. pair_potentials moved to pair_potentials

--- models/potentials/__init__.py
+++ models/potentials/__init__.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from model_builder.models.structure import contacts as cts
+from model_builder.models.potentials.pair_potentials import *
 
 
 #POTENTIALS = {1:LJ12Potential,
@@ -22,15 +23,19 @@ def _hash_numpy_array(x):
     hash_value ^= hash(x.data.tobytes())
     return hash_value
 
-class Potentials(object):
+class Hamiltonian(object):
     """Mode Hamiltonian"""
-    pair_potentials = {2:LJ1210Potential}
+    pair_potentials = \
+        {2:LJ1210Potential
+        5:TanhRepPotential
+        }
 
     def __init__(self):
         self._bonds = []
         self._angles = []
         self._dihedrals = []
         self._pairs = []
+        #self._special = []  # Add?
 
     @property
     def n_bonds(self):
@@ -85,7 +90,7 @@ class Potentials(object):
         if p not in self._pairs:
             self._pairs.append(p)
         else:
-            print "warning"
+            print "warning: pair already has this interaction {}. skipping.".format(p.describe())
 
     def add_sbm_contacts(self, Model):
         residue_contacts = cts.residue_contacts(Model.ref_traj)
@@ -99,113 +104,22 @@ class Potentials(object):
             r0 = np.linalg.norm(xyz[atm1.index,:] - xyz[atm2.index,:])
             self.add_pair(code, atm1, atm2, eps, r0)
 
-############################################################################
-# Pair potentials
-############################################################################
+    def define_contact_group(self, label, pairs):
+        # Use this to define a group of contacts by a label.
+        # The label can later be used to get their energy. 
+        # e.g. 'native' [[1,10],[2,10]]
+        pass 
 
-class PairPotential(object):
-     
-    def __init__(self, atmi, atmj):
-        self.atmi = atmi
-        self.atmj = atmj
+    def calc_contact_group_energy(self, label, traj):
+        # Calculate the energy of a group of contacts
+        pass
 
-    def describe(self):
-        """interaction description"""
-        return "%s:%12s%12s" % (self.prefix_label, self.atmi, self.atmj)
+    def select_parameters(self):
+        # Identify parameters by:
+        #   - parameter type: eps, r0, 
+        #   - interaction type: bonds, angles, etc.
+        pass
 
-    def __hash__(self):
-        hash_value = hash(self.prefix_label)
-        hash_value ^= hash(self.atmi.index)
-        hash_value ^= hash(self.atmj.index)
-        return hash_value
-
-    def __eq__(self, other):
-        return self.__hash__() == other.__hash__()
-
-class LJPotential(PairPotential):
-    
-    def __init__(self, atmi, atmj, eps, r0):
-        PairPotential.__init__(self, atmi, atmj)
-        self.eps = eps
-        self.r0 = r0
-
-    def __hash__(self):
-        hash_value = PairPotential.__hash__(self)
-        hash_value ^= hash(self.eps)
-        hash_value ^= hash(self.r0)
-        return hash_value
-
-class LJ12Potential(LJPotential):
-
-    def __init__(self, atmi, atmj, eps, r0):
-        LJPotential.__init__(self, atmi, atmj, eps, r0) 
-        self.prefix_label = "LJ12"
-
-    def V(self, r):
-        return self.eps*self.dVdeps(r)
-
-    def dVdeps(self, r):
-        x = self.r0/r
-        return x**12
-
-    def dVdr(self, r):
-        return self.eps*self.d2Vdrdeps(r)
-
-    def d2Vdrdeps(self, r):
-        x = self.r0/r
-        return (-60./self.r0)*((x**13) - (x**11))
-
-class LJ1210Potential(LJPotential):
-    
-    def __init__(self, atmi, atmj, r0, eps):
-        LJPotential.__init__(self, atmi, atmj, r0, eps)
-        self.prefix_label = "LJ1210"
-
-    def V(self, r):
-        return self.eps*self.dVdeps(r)
-
-    def dVdeps(self, r):
-        x = self.r0/r
-        return 5.*(x**12) - 6.*(x**10)
-
-    def dVdr(self, r):
-        return self.eps*self.d2Vdrdeps(r)
-
-    def d2Vdrdeps(self, r):
-        x = self.r0/r
-        return (-60./self.r0)*((x**13) - (x**11))
-
-class TanhRepPotential(PairPotential):
-    
-    def __init__(self, atmi, atmj, eps, r0, width):
-        PairPotential.__init__(self, atmi, atmj)
-        self.prefix_label = "TANHREP"
-        self.eps = eps
-        self.r0 = r0
-        self.width = width
-
-    def V(self,r):
-        return self.eps*dVdeps(r) 
-
-    def dVdeps(self, r):
-        alpha = 1./self.width
-        r0prime = self.r0 + self.width
-        return 0.5*(np.tanh(-alpha*(r - r0prime)) + 1.)
-
-    def dVdr(self, r):
-        return self.eps*d2Vdrdeps(r) 
-
-    def d2Vdrdeps(self, r):
-        alpha = 1./self.width      
-        r0prime = self.r0 + self.width
-        return -0.5*alpha*(1. - (np.tanh(-alpha*(r - r0prime)))**2)
-
-    def __hash__(self):
-        hash_value = PairPotential.__hash__(self)
-        hash_value ^= hash(self.eps)
-        hash_value ^= hash(self.r0)
-        hash_value ^= hash(self.width)
-        return hash_value
 
 if __name__ == "__main__":
     pass
--- models/potentials/pair_potentials.py
+++ models/potentials/pair_potentials.py
@@ -1,234 +1,111 @@
-""" Utilities for nonbonded/pairwise potential terms.
-
-Potentials to add: 
-- Repulsive tanh function by Ryan Cheng to complement Gaussian  DONE
-- Attractive and repulsive LJ126 potentials DONE
-- Compound functions i.e. Heiko's product functions  DONE
-- Compound (product) functions with Gaussian barrier. 
-
-- Add second derivatives
-
-"""
-
 import numpy as np
 
-##############################################################################
-# Utilities and wrappers
-##############################################################################
+# TODO: Add derivatives of potential w.r.t. all parameters
 
-def get_pair_potential(code):
-    """ Returns pairwise potential function"""
-    potential = {1:LJ12,
-                 2:LJ1210,
-                 3:LJ1210rep,
-                 4:Gaussian,
-                 5:Cheng_rep,
-                 6:LJ126,
-                 7:LJ126rep,
-                 8:compound_LJ12_Gaussian,
-                 9:compound_LJ12_double_Gaussian,
-                 10:compound_double_Gaussian,
-                 11:FRET_Efficiency}[code]
-    return potential
+############################################################################
+# Pair potentials
+############################################################################
 
-def get_pair_potential_deriv(code):
-    """ Returns derivative of pairwise potential function"""
-    potential = {1:LJ12_deriv,
-                 2:LJ1210_deriv,
-                 3:LJ1210rep_deriv,
-                 4:Gaussian_deriv,
-                 5:Cheng_rep_deriv,
-                 6:LJ126_deriv,
-                 7:LJ126rep_deriv,
-                 8:compound_LJ12_Gaussian_deriv,
-                 9:compound_LJ12_double_Gaussian_deriv,
-                 10:compound_double_Gaussian_deriv,
-                 11:FRET_Efficiency_deriv}[code]
-    return potential
+class PairPotential(object):
+     
+    def __init__(self, atmi, atmj):
+        self.atmi = atmi
+        self.atmj = atmj
 
-def get_switched_pair_potential(code):
-    """ Returns the "switched" pairwise potential function. NOT DONE/USED"""
-    potential = {1:LJ12,2:LJ1210,3:LJ1210rep,4:Gaussian,5:Cheng_rep}[code]
-    return potential
+    def describe(self):
+        """interaction description"""
+        return "%s:%12s%12s" % (self.prefix_label, self.atmi, self.atmj)
 
-def wrap_pairwise(old_pairwise,*args):
-    """Wrap pairwise function so only r needs to be passed 
+    def __hash__(self):
+        hash_value = hash(self.prefix_label)
+        hash_value ^= hash(self.atmi.index)
+        hash_value ^= hash(self.atmj.index)
+        return hash_value
 
-    Parameters:
-    -----------
-    old_pairwise : function
-        A function that takes one distance (r) and a variable number of
-        positional arguments depends on (such as equilibrium distance or width
-        of well).
-    args : tuple
-        A tuple that contains values of position arguments for old_pairwise
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
 
-    Returns:
-    --------
-    new_pairwise : function
-        A function that only depends on the distance, with the remaining 
-        positional parameters hard coded.
-    """
-    def new_pairwise(r):
-        return old_pairwise(r,*args)
-    return new_pairwise
+class LJPotential(PairPotential):
+    
+    def __init__(self, atmi, atmj, eps, r0):
+        PairPotential.__init__(self, atmi, atmj)
+        self.eps = eps
+        self.r0 = r0
 
-def wrap_sum_of_pairwise(*args):
-    """ Returns function that is sum of args functions. 
+    def __hash__(self):
+        hash_value = PairPotential.__hash__(self)
+        hash_value ^= hash(self.eps)
+        hash_value ^= hash(self.r0)
+        return hash_value
 
-    Inputs:
-        A list of functions that each depend on a distance (r).
-    Returns:
-        A function that evaluates the sum of the inputted functions.
-    The returned function is also depends on the distance (r).
-    NOT USED
-    """
-    def sum_pairwise(r):
-        return sum(np.array([ args[i](r) for i in range(len(args)) ]))
-    return sum_pairwise
+class LJ12Potential(LJPotential):
 
-def wrap_sum_of_pairwise_with_coefficients(*args):
-    """ Returns function that is sum of args functions. 
+    def __init__(self, atmi, atmj, eps, r0):
+        LJPotential.__init__(self, atmi, atmj, eps, r0) 
+        self.prefix_label = "LJ12"
 
-    Inputs:
-        A list where the first half is a list of numbers and the second
-    half is a list of functions that each depend on a distance (r).
+    def V(self, r):
+        return self.eps*self.dVdeps(r)
 
-    Returns:
-        A function that evaluates the weighted sum of the inputted functions.
-    The weights are the numbers in the first half of the inputted list.
-    The returned function is also depends on the distance (r).
-    NOT USED
-    """
-    def sum_pairwise_with_coefficients(r):
-        return sum(np.array([ args[i]*args[i+(len(args)/2)](r) for i in range(len(args)/2) ]))
-    return sum_pairwise_with_coefficients
+    def dVdeps(self, r):
+        x = self.r0/r
+        return x**12
 
-##############################################################################
-# Pairwise interaction potentials
-##############################################################################
+    def dVdr(self, r):
+        return self.eps*self.d2Vdrdeps(r)
 
-def LJ12(r,r0):
-    x = r0/r
-    V = x**12
-    return V
+    def d2Vdrdeps(self, r):
+        x = self.r0/r
+        return (-60./self.r0)*((x**13) - (x**11))
 
-def LJ12_deriv(r,r0):
-    x = r0/r
-    V = (-12./r0)*(x**13)
-    return V
+class LJ1210Potential(LJPotential):
+    
+    def __init__(self, atmi, atmj, r0, eps):
+        LJPotential.__init__(self, atmi, atmj, r0, eps)
+        self.prefix_label = "LJ1210"
 
-def LJ1210(r,r0):
-    x = r0/r
-    V = 5.*(x**12) - 6.*(x**10)
-    return V
+    def V(self, r):
+        return self.eps*self.dVdeps(r)
 
-def LJ1210_deriv(r,r0):
-    x = r0/r
-    V = (-60./r0)*((x**13) - (x**11))
-    return V
+    def dVdeps(self, r):
+        x = self.r0/r
+        return 5.*(x**12) - 6.*(x**10)
 
-def LJ1210rep(r,r0):
-    x = r0/r
-    V = np.zeros(x.shape,float)
-    V[x > 1] = (5.*(x[x > 1]**12) - 6.*(x[x > 1]**10) + 2.)
-    V[x <= 1] = -(5.*(x[x <= 1]**12) - 6.*(x[x <= 1]**10))
-    return V
+    def dVdr(self, r):
+        return self.eps*self.d2Vdrdeps(r)
 
-def LJ1210rep_deriv(r,r0):
-    x = r0/r
-    V = np.zeros(x.shape,float)
-    V[x > 1] = -60.*(1./r0)*(x[x > 1]**13 - x[x > 1]**11)
-    V[x <= 1] = 60.*(1./r0)*(x[x <= 1]**13 - x[x <= 1]**11)
-    return V
+    def d2Vdrdeps(self, r):
+        x = self.r0/r
+        return (-60./self.r0)*((x**13) - (x**11))
 
-def Gaussian(r,r0,width):
-    V = -np.exp(-((r - r0)**2)/(2.*(width**2)))
-    return V
+class TanhRepPotential(PairPotential):
+    
+    def __init__(self, atmi, atmj, eps, r0, width):
+        PairPotential.__init__(self, atmi, atmj)
+        self.prefix_label = "TANHREP"
+        self.eps = eps
+        self.r0 = r0
+        self.width = width
 
-def Gaussian_deriv(r,r0,width):
-    V = ((r - r0)/(width**2))*np.exp(-((r - r0)**2)/(2.*(width**2)))
-    return V
+    def V(self,r):
+        return self.eps*dVdeps(r) 
 
-def Cheng_rep(r,r0,width):
-    alpha = 1./width      
-    r0prime = r0 + width
-    V = 0.5*(np.tanh(-alpha*(r - r0prime)) + 1.)
-    return V
+    def dVdeps(self, r):
+        alpha = 1./self.width
+        r0prime = self.r0 + self.width
+        return 0.5*(np.tanh(-alpha*(r - r0prime)) + 1.)
 
-def Cheng_rep_deriv(r,r0,width):      
-    alpha = 1./width      
-    r0prime = r0 + width
-    V = -0.5*alpha*(1. - (np.tanh(-alpha*(r - r0prime)))**2)
-    return V
+    def dVdr(self, r):
+        return self.eps*d2Vdrdeps(r) 
 
-def LJ126(r,r0):
-    x = r0/r
-    V = (x**12) - 2.*(x**6)
-    return V
+    def d2Vdrdeps(self, r):
+        alpha = 1./self.width      
+        r0prime = self.r0 + self.width
+        return -0.5*alpha*(1. - (np.tanh(-alpha*(r - r0prime)))**2)
 
-def LJ126_deriv(r,r0):
-    x = r0/r
-    V = (-12./r0)*((x**13) - (x**7))
-    return V
-
-def LJ126rep(r,r0):
-    """ ONLY PLACEHOLDER. NOT DONE """
-    x = r0/r
-    V = (x**12) - 2.*(x**6)
-    return V
-
-def LJ126rep_deriv(r,r0):
-    """ ONLY PLACEHOLDER. NOT DONE """
-    x = r0/r
-    V = (-12./r0)*((x**13) - (x**7))
-    return V
-
-
-##############################################################################
-# Compound pairwise potentials. 
-##############################################################################
-
-def compound_LJ12_Gaussian(r,rNC,r0,width0):
-    return LJ12(r,rNC)*(1. + Gaussian(r,r0,width0))
-
-def compound_LJ12_Gaussian_deriv(r,rNC,r0,width0):
-    return LJ12_deriv(r,rNC)*(1. + Gaussian(r,r0,width0)) + LJ12(r,rNC)*Gaussian_deriv(r,r0,width0)
-
-def compound_LJ12_double_Gaussian(r,rNC,r0,width0,r1,width1):
-    # Not tested in simulation
-    return LJ12(r,rNC)*(1. + Gaussian(r,r0,width0))*(1. + Gaussian(r,r1,width1))
-
-def compound_LJ12_double_Gaussian_deriv(r,rNC,r0,width0,r1,width1):
-    return LJ12_deriv(r,rNC)*(1. + Gaussian(r,r0,width0))*(1. + Gaussian(r,r1,width1)) + \
-           LJ12(r,rNC)*Gaussian_deriv(r,r0,width0)*(1. + Gaussian(r,r1,width1)) + \
-           LJ12(r,rNC)*(1. + Gaussian(r,r0,width0))*Gaussian_deriv(r,r1,width1)
-
-def compound_double_Gaussian(r,rNC,r0,width0,r1,width1):
-    return Gaussian(r,r0,width0)*(1. + Gaussian(r,r1,width1))
-
-def compound_double_Gaussian_deriv(r,rNC,r0,width0,r1,width1):
-    return Gaussian_deriv(r,r0,width0)*(1. + Gaussian(r,r1,width1)) +\
-           Gaussian(r,r0,width0)*Gaussian_deriv(r,r1,width1)
-
-##############################################################################
-# Special pairwise potentials. 
-##############################################################################
-
-def FRET_Efficiency(r,r0):
-    x = r/r0
-    V = 1./(1. + x**6)
-    return V
-
-def FRET_Efficiency_deriv(r,r0):
-    x = r/r0
-    V = (-6./r0)*(x**5)/((1. + x**6)**2)
-    return V
-
-def FRET_Efficiency_moment(r,r0,eff_mean,moment):
-    V = (FRET_Efficiency(r,r0) - eff_mean)**moment
-    return V
-
-def FRET_Efficiency_moment_deriv(r,r0,eff_mean,moment):
-    V = moment*((FRET_Efficiency(r,r0) - eff_mean)**(moment - 1))*FRET_Efficiency_deriv(r,r0)
-    return V
+    def __hash__(self):
+        hash_value = PairPotential.__hash__(self)
+        hash_value ^= hash(self.eps)
+        hash_value ^= hash(self.r0)
+        hash_value ^= hash(self.width)
+        return hash_value
