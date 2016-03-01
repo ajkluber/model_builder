83705b091f801a59dacbb06c7f852075e61d2ddc
Alexander Kluber <ajkluber@rice.edu>
2016-03-01 13:30:15 -0600

    [models] put Model in __init__

--- models/__init__.py
+++ models/__init__.py
@@ -12,6 +12,7 @@ Classes:
 References:
 
 """
+import numpy as np
 
 import CoarseGrainedModel
 import bonded_potentials
@@ -22,5 +23,39 @@ import prep_Gaussian_pairwise
 
 import structure
 import potentials
+import output
 
-import Model
+#import Model
+
+from model_builder.models.structure import mappings as mpg
+from model_builder.models.structure import contacts as cts
+from model_builder.models import potentials as ptl
+
+'''
+A Model consists of:
+- a structure mapping
+- a Hamiltonian
+    - a set of parameterized interaction potentials
+'''
+
+class Model(object):
+
+    def __init__(self, topology=None, traj=None):
+        if (topology is None) and (traj is not None):
+            topology = traj.top
+        self.structure_mapping = mpg.CalphaMapping(topology)  
+        self.potentials = ptl.Hamiltonian()
+
+    def set_reference(self, traj):
+        self.ref_traj_aa = traj[0]
+        self.ref_traj = self.structure_mapping.map_traj(traj[0])
+
+    def describe(self):
+        pass 
+
+    def add_sbm_contacts(self):
+        self.potentials.add_sbm_contacts(self)
+
+    def map_traj(self, traj):
+        self.structure_mapping.map_traj(traj)
+
