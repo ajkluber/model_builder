fixed bugs with angle energy calculation

examples/butane/run_butane.py
@@ -47,10 +47,10 @@ def butane_toy_model():
 
     # Add angle interactions
     model.Hamiltonian._add_angle("HARMONIC_ANGLE", top.atom(0), top.atom(1),
-                                    top.atom(2), 20*(np.pi/180)**2, 90)
+                                    top.atom(2), 20, np.pi/2)
 
     model.Hamiltonian._add_angle("HARMONIC_ANGLE", top.atom(1), top.atom(2),
-                                    top.atom(3), 20*(np.pi/180)**2, 90)
+                                    top.atom(3), 20, np.pi/2)
 
     # Add dihedral interaction
     model.Hamiltonian._add_dihedral("COSINE_DIHEDRAL", top.atom(0), top.atom(1),

models/potentials/__init__.py
@@ -239,7 +239,7 @@ class Hamiltonian(object):
         sum : bool (opt.)
             If sum=True return the total energy.
         """
-        theta = (180./np.pi)*md.compute_angles(traj, self._angle_idxs)
+        theta = md.compute_angles(traj, self._angle_idxs)
         if sum:
             Eangle = np.zeros(traj.n_frames, float)
         else: