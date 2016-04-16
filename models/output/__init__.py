063bdae01a2a6394f488ff8b4be555563fb20c81
Alexander Kluber <ajkluber@rice.edu>
2016-04-16 18:28:29 -0500

    [output] Adding support for LAMMPS topology file

--- models/output/__init__.py
+++ models/output/__init__.py
@@ -282,3 +282,90 @@ class GromacsFiles(object):
 
         self.topfile = top
 
+
+class LammpsFiles(object):
+
+    def __init__(self, model):
+        self.model = model
+        self.version = version
+
+        # check compatibility of interactions with this version
+        # of gromacs
+
+        # check for interactions that need to be tabled.
+
+        # determine the right lookup numbers for each interactions 
+        self._bond_funcs = {"HARMONIC_BOND":1}
+        self._angle_funcs = {"HARMONIC_ANGLE":1}
+        self._dihedral_funcs = {"COSINE_DIHEDRAL":1,
+                                "HARMONIC_DIHEDRAL":2}
+
+        self._supported_pair_potentials = ["LJ12", "LJ1210",
+                                            "GAUSSIAN", "LJ12GAUSSIAN"]
+
+    def generate_topology(self):
+
+        # 
+
+        mapping = self.model.mapping
+        top = self.model.mapping.top
+        if hasattr(model, "starting_traj"):
+            xyz = model.starting_traj.xyz[0]
+        elif hasattr(model, "ref_traj"):
+            xyz = model.ref_traj.xyz[0]
+        else:
+            raise AttributeError("need to set intial conditions (ref_traj or starting_traj) to write")
+        
+        top_string = "LAMMPS written by model_builder\n"
+        top_string += "{:>12d}  atoms\n".format(top.n_atoms)
+        top_string += "{:>12d}  bonds\n".format(top.n_bonds)
+        top_string += "{:>12d}  angles\n".format(0)
+        top_string += "{:>12d}  dihedrals\n".format(0)
+        top_string += "{:>12d}  impropers\n\n".format(0)
+
+        top_string += "{:>12d}  atom types\n".format(mapping.n_atomtypes)
+        top_string += "{:>12d}  bond types\n\n".format(top.n_bonds)
+
+        top_string += "{:8.1f}{:8.1f}xlo xhi\n".format(-200., 200.)
+        top_string += "{:8.1f}{:8.1f}ylo yhi\n".format(-200., 200.)
+        top_string += "{:8.1f}{:8.1f}zlo zhi\n\n".format(-200., 200.)
+
+        top_string += "Masses\n\n"
+        atomtype_map = {}
+        for i in range(mapping.n_atomtypes):
+            atomtype_map[mapping.atomtypes[i]] = i + 1
+            top_string += "{:>12d}    {:.2f}\n".format(i + 1, mapping.atomtypes[i].mass)
+        
+        top_string += "\n"
+        top_string += "Atoms\n\n"
+        for i in range(top.n_atoms):
+            atom = top.atom(i)
+            atom_type = mapping.atoms[i]
+            mass = atom_type.mass
+            charge = atom_type.charge
+            type_idx = atomtype_map[atom_type]
+            top_string += "{:>12d}{:>5d}{:>5d}{:>5d}{:>6f} {:>10f}{:>10f}{:>10f}\n".format(
+                        i + 1, atom.residue.chain.index + 1, atom.residue.index + 1, 
+                        type_idx, charge, xyz[i,0], xyz[i,1], xyz[i,2])
+        top_string += "\n"
+
+        top_string += "Bond Coeffs\n"
+        for i in range(model.Hamiltonian._bonds):
+            bond = model.Hamiltonian._bonds[i]
+            top_string += "{:>12d}{:>6f}{:>6f}\n".format(i + 1, bond.kb, bond.r0*10.)
+        top_string += "\n"
+
+        top_string += "Bonds\n"
+        for i in range(model.Hamiltonian._bonds):
+            bond = model.Hamiltonian._bonds[i]
+            top_string += "{:>12d}{:>5d}{:>5d}{:>5d}\n".format(i + 1, i + 1, bond.atmj.index + 1, bond.atmj.index + 1)
+        top_string += "\n"
+
+        self.topfile = top_string
+
+
+
+
+
+
+
