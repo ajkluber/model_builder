0425e2b44edee4b887242204ee4bff5c3adaed00
Alexander Kluber <ajkluber@rice.edu>
2016-05-20 11:07:21 +0200

    [potentials] Added comments to awsem potentials

--- models/potentials/awsem.py
+++ models/potentials/awsem.py
@@ -1,8 +1,50 @@
 import numpy as np
 
+##############################################################################
+# Utility functions
+##############################################################################
+
 def theta(r, nu, r_min, r_max):
     return 0.25*(1. + np.tanh(nu*(r - r_min)))*(1. + np.tanh(nu*(r_max - r)))
 
+##############################################################################
+# Backbone terms
+##############################################################################
+class Chi(object):
+
+    def __init__(self, lambda_chi=1., chi_0=-0.83):
+        """Sidechain chirality potential
+
+        Parameters
+        ----------
+        lambda_chi : opt, float
+            The overall strength of the term in the Hamiltonian.
+        nu : opt, float
+
+        """
+        self.lambda_chi
+        self.chi_0
+
+    def V(self, C_xyz, CA_xyz, CB_xyz, N_xyz):
+        """
+        Parameters
+        ----------
+        C_xyz : np.ndarray (3)
+            Coordinates of the backbone C atom.
+        CA_xyz : np.ndarray (3)
+            Coordinates of the backbone CA atom.
+        CB_xyz : np.ndarray (3)
+            Coordinates of the CB atom.
+        N_xyz : np.ndarray (3)
+            Coordinates of the backbone N atom.
+        """
+        v1 = CA_xyz - C_xyz; v2 = N_xyz - CA_xyz; v3 = CA_xyz - CB_xyz
+        chi = np.dot(np.cross(v1, v2), v3)
+        return self.lambda_chi*((chi - chi_0)**2)
+
+##############################################################################
+# Contact terms
+##############################################################################
 
 class DirectContact(object):
 
@@ -22,8 +64,27 @@ class DirectContact(object):
         return theta(r, self.nu, self.r_min, self.max)
 
 class WaterMediatedContact(object):
-
+    
     def __init__(self, lambda_water=1., nu=50., nu_sigma=70., r_min=0.65, r_max=0.95):
+        """Water- or protein-mediated contact interaction
+        
+        Parameters
+        ----------
+        lambda_water : opt, float
+            The overall strength of the term in the Hamiltonian.
+        nu : opt, float
+            Coefficient that sets how quickly the mediated well switches
+            between on and off.
+        nu_sigma : opt, float
+            Coefficient that sets how fast the contact switches from 
+            water-mediated to protein-mediated based on the local densities of
+            the residues involved.
+        r_min : opt, float
+            The minimum of the mediated well.
+        r_max : opt, float
+            The maximum of the mediated well.
+            
+        """
         self.lambda_water = lambda_water
         self.nu = nu
         self.nu_sigma = nu_sigma
@@ -31,6 +92,25 @@ class WaterMediatedContact(object):
         self.r_max = r_max
 
     def V(self, r, rhoi, rhoj, gamma_water, gamma_protein): 
+        """"Water- or protein-mediated contact interaction
+        
+        Parameters
+        ----------
+        r : np.ndarray (n_frames)
+            Distance between sidechain CB's involved in interaction. If one or
+            both residues are glycine then the CA atom is used instead.
+        rhoi : np.ndarray (n_frames)
+            Local protein density around residue i.
+        rhoj : np.ndarray (n_frames)
+            Local protein density around residue j.
+        gamma_water : float
+            The strength of the water-mediated interaction between residues i
+            and j.
+        gamma_protein : float
+            The strength of the protein-mediated interaction between residues i
+            and j.
+        
+        """"
         return self.lambda_water*(gamma_water*self.dVdgamma_water(r, rhoi, rhoj) +\
                                 gamma_protein*self.dVdgamma_protein(r, rhoi, rhoj))
 
@@ -47,8 +127,27 @@ class WaterMediatedContact(object):
         return 0.25*(1. - np.tanh(self.nu_sigma*(rhoi - self.rho_0)))*(1. - np.tanh(self.nu_sigma*(rhoj - self.rho_0)))
     
 class Burial(object):
-    """One-body burial potential"""
     def __init__(self, lambda_burial=1., nu=40., rho1_lims=[0.0, 0.3], rho2_lims=[0.3, 0.6], rho3_lims=[0.6, 0.9]):
+        """One-body burial potential
+
+        The burial potential assigns an energy for being in (1) low, (2)
+        medium, or (3) high protein density. 
+
+        Parameters
+        ----------
+        lambda_burial : opt, float
+            The overall strength of the term in the Hamiltonian.
+        nu : opt, float
+            Coefficient that sets how quickly the burial well switches between on and off.
+        rho1_lims : opt, list (2)
+            The density limits of the low-density well.
+        rho2_lims : opt, list (2)
+            The density limits of the medium-density well.
+        rho3_lims : opt, list (2)
+            The density limits of the high-density well.
+
+        """
+
         self.lambda_burial = lambda_burial
         self.nu = nu
         self.rho_lims = [ rho1_lims, rho2_lims, rho3_lims ]
@@ -72,29 +171,6 @@ class Burial(object):
 
     def burial_theta(self, rhoi, nu, rho_min, rho_max):
         return  0.5*np.tanh(nu*(rhoi - rho_min))*np.tanh(nu*(rhoi- rho_max))
-
-class Chi(object):
-
-    def __init__(self, lambda_chi=1., chi_0=-0.83):
-        self.lambda_chi
-        self.chi_0
-
-    def V(self, C_xyz, CA_xyz, CB_xyz, N_xyz):
-        """
-        Parameters
-        ----------
-        C_xyz : np.ndarray (3)
-            Coordinates of the backbone C atom.
-        CA_xyz : np.ndarray (3)
-            Coordinates of the backbone CA atom.
-        CB_xyz : np.ndarray (3)
-            Coordinates of the CB atom.
-        N_xyz : np.ndarray (3)
-            Coordinates of the backbone N atom.
-        """
-        v1 = CA_xyz - C_xyz; v2 = N_xyz - CA_xyz; v3 = CA_xyz - CB_xyz
-        chi = np.dot(np.cross(v1, v2), v3)
-        return self.lambda_chi*((chi - chi_0)**2)
 
 AWSEM_POTENTIALS = {"DIRECT":DirectContact,
                 "WATER":WaterMediatedContact,
