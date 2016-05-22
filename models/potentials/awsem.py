da48c3192b305a683bf4505645f90731f8618747
Alexander Kluber <ajkluber@rice.edu>
2016-05-22 20:46:39 +0200

    [potentials] drafted awsem helical interaction

--- models/potentials/awsem.py
+++ models/potentials/awsem.py
@@ -7,11 +7,15 @@ import numpy as np
 def theta(r, nu, r_min, r_max):
     return 0.25*(1. + np.tanh(nu*(r - r_min)))*(1. + np.tanh(nu*(r_max - r)))
 
+def sigma_water(self, rhoi, rhoj, nu_sigma, rho_0):
+    return 0.25*(1. - np.tanh(nu_sigma*(rhoi - rho_0)))*(1. - np.tanh(nu_sigma*(rhoj - rho_0)))
+
 ##############################################################################
 # Backbone terms
 ##############################################################################
 
 class Spring(object):
+    # NOT TESTED
     def __init__(self, k, r0):
         """Harmonic potential 
         
@@ -30,7 +34,7 @@ class Spring(object):
         return self.k*((r - self.r0)**2)
 
 class Chi(object):
-
+    # NOT TESTED
     def __init__(self, lambda_chi=1., chi_0=-0.83):
         """Sidechain chirality potential
 
@@ -65,7 +69,7 @@ class Chi(object):
         return self.lambda_chi*((chi - chi_0)**2)
 
 class Rama(object):
-
+    # NOT TESTED
     def __init__(self, lambda_rama=2., 
             W=[1.3149, 1.32016, 1.0264], sigma=[15.398, 49.0521, 49.0954],
             omega_phi=[0.15, 0.25, 0.65], phi0=[-1.74, -1.265, 1.041], 
@@ -84,6 +88,39 @@ class Rama(object):
                     self.omega_phi[i]*(np.cos(phi - self.phi0[i]) - 1.)**2 +\
                     self.omega_psi[i]*(np.cos(psi - self.psi0[i]) - 1.)**2))
         return V
+
+class Helix(object):
+
+    def __init__(self, lambda_helix=1.5,
+            gamma_protein=2., gamma_water=-1., nu=70. nu_sigma=7.,
+            rho_0=3., r_ON=0.298, r_OH=0.206, sigma_ON=0.068, sigma_OH=0.076)
+    """Alpha helical interaction
+    
+    Parameters
+    ----------
+    lambda_direct : opt, float
+        The overall strength of the term in the Hamiltonian.
+    
+    """
+    self.lambda_helix = lambda_helix
+    self.gamma_protein = gamma_protein
+    self.gamma_water = gamma_water
+    self.nu = nu
+    self.nu_sigma = nu_sigma
+    self.rho_0 = rho_0
+    self.r_ON_0 = r_ON
+    self.r_OH_0 = r_OH
+    self.sigma_ON = sigma_ON
+    self.sigma_OH = sigma_OH
+
+    def V(self, r_ON, r_OH, rhoi, rhoj, res_avg_helix):
+        return res_avg_helix*self.gauss_well(r_ON, r_OH)*(
+                self.gamma_water*sigma_water(rhoi, rhoj, self.nu_sigma, self.rho_0) +\
+                self.gamma_protein*(1. - sigma_water(rhoi, rhoj, self.nu_sigma, self.rho_0)))
+    
+    def gauss_well(self, r_ON, rOH):  
+        return -self.lambda_helix*np.exp(-(((r_ON - self.r_ON_0)**2)/(2.*(self.sigma_ON**2))) -\
+                                          (((r_OH - self.r_OH_0)**2)/(2.*(self.sigma_OH**2)))
 
 ##############################################################################
 # Contact terms
@@ -173,19 +210,17 @@ class WaterMediatedContact(object):
                                 gamma_protein*self.dVdgamma_protein(r, rhoi, rhoj))
 
     def dVdgamma_water(self, r, rhoi, rhoj):
-        return self.theta_II(r)*self.sigma_water(rhoi, rhoj)
+        return self.theta_II(r)*sigma_water(rhoi, rhoj, self.nu_sigma, self.rho_0)
 
     def dVdgamma_protein(self, r, rhoi, rhoj):
-        return self.theta_II(r)*(1. - self.sigma_water(rhoi, rhoj))
+        return self.theta_II(r)*(1. - sigma_water(rhoi, rhoj, self.nu_sigma, self.rho_0))
 
     def theta_II(self, r):
         return theta(r, self.nu, self.r_min, self.r_max)
 
-    def sigma_water(self, rhoi, rhoj):
-        return 0.25*(1. - np.tanh(self.nu_sigma*(rhoi - self.rho_0)))*(1. - np.tanh(self.nu_sigma*(rhoj - self.rho_0)))
     
 class Burial(object):
-    def __init__(self, lambda_burial=1., nu=40., rho1_lims=[0.0, 0.3], rho2_lims=[0.3, 0.6], rho3_lims=[0.6, 0.9]):
+    def __init__(self, lambda_burial=1., nu=4., rho1_lims=[0.0, 3.], rho2_lims=[3., 6.], rho3_lims=[6., 9.]):
         """One-body burial potential
 
         The burial potential assigns an energy for being in (1) low, (2)
@@ -206,9 +241,14 @@ class Burial(object):
 
         """
 
+        self.label = "BURIAL"
         self.lambda_burial = lambda_burial
         self.nu = nu
         self.rho_lims = [ rho1_lims, rho2_lims, rho3_lims ]
+
+    def describe(self):
+        return "{} potential : lambda_burial {} nu = {}, rho_lims = {}".format(
+                self.label, self.lambda_burial, self.nu, self.rho_lims.__repr__())
 
     def V(self, rhoi, gamma_burials):
         """Burial potential
@@ -229,9 +269,11 @@ class Burial(object):
         return V
 
     def burial_theta(self, rhoi, nu, rho_min, rho_max):
-        return  0.5*np.tanh(nu*(rhoi - rho_min))*np.tanh(nu*(rhoi- rho_max))
+        #return  0.5*np.tanh(nu*(rhoi - rho_min))*np.tanh(nu*(rhoi- rho_max))
+        return  0.5*(np.tanh(nu*(rhoi - rho_min)) + np.tanh(nu*(rho_max - rhoi)))
 
 AWSEM_POTENTIALS = {"DIRECT":DirectContact,
                 "WATER":WaterMediatedContact,
                 "BURIAL":Burial, 
+                "HELIX":Helix,
                 "CHI":Chi}
